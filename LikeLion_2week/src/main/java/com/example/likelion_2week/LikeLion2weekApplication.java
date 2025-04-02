975fa27879fa8c1b6cdec45a0a1c554cc4adcdc8
sungmlee <ler0101@naver.com>
2025-04-02 13:12:19 +0900

    git test

---
+++ LikeLion_2week/src/main/java/com/example/likelion_2week/LikeLion2weekApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelion_2week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLion2weekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeLion2weekApplication.class, args);
+    }
+
+}
---
+++ LikeLion_2week/src/test/java/com/example/likelion_2week/LikeLion2weekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelion_2week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLion2weekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
